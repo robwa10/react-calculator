53cb26b8a496bf098342ab77ca5f70a114456539
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-03-17 19:58:22 +0000

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
-import NumberPad from './components/numberpad'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import Header from './components/header'
+import ExpressionResultContainer from './containers/expression-result-container'
+import InputContainer from './containers/input-container'
+
+let store = createStore(reducer)
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NumberPad />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header />
+          <ExpressionResultContainer />
+          <InputContainer />
+        </div>
+      </Provider>
     )
   }
 }
