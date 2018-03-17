1c0bb6a9aca1eebd8164837fc4a72dceb242309a
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-03-17 00:25:45 +0000

    Add DELETE_ELEMENT

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,17 @@ import * as types from '../actions/ActionTypes'
 import parseString from '../helpers/parse-string'
 import calculate from '../helpers/calculate'
 
+// ********************
+// Small helper functions
+
 const concatString = (state, data) => {
   const x = state // Copy of state to mutate
   return x + data
+}
+
+const removeLastElement = (state) => {
+  let copy = state // Copy of state to mutate
+  return copy.slice(0, -1)
 }
 
 const getResult = (input, data) => {
@@ -13,10 +21,15 @@ const getResult = (input, data) => {
   return calculate(parsed)
 }
 
+// ********************
+// Reducers
+
 export const inputString = (state = '', action) => {
   switch (action.type) {
     case types.INPUT_STRING:
       return concatString(state, action.data)
+    case types.DELETE_ELEMENT:
+      return removeLastElement(state)
     case types.CALCULATE_RESULT:
       return action.data === 'Bad Expression' ? state : action.data
     default:
