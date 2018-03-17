95eae97341424dc12317973a8c6e71925ab07508
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-03-17 00:26:27 +0000

    Add check for decimal number

--- src/helpers/parse-string.js
+++ src/helpers/parse-string.js
@@ -2,7 +2,7 @@ const parseString = (s) => {
   let myArray = []
   let tempString = '' // Place to hold numbers in case they are more than one digit
   for (var i = 0; i < s.length; i++) {
-    if (isNaN(s[i])) { // Check if s[i] is an operator or paren
+    if (isNaN(s[i]) && s[i] !== '.') { // Check if s[i] is an operator or parens
       if (s[i] === '-' && (i === 0 || s[i - 1] === '(')) { // Check if it's a minus or negative number
         tempString = tempString + s[i] // Add negative before adding number
       } else if (tempString.length !== 0) { // Check if a number has already been added
