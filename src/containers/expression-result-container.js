df4521033bf5ddc45a862a405602d62c6d974f56
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-03-18 17:58:01 +0000

    Change font-size conditionally

--- src/containers/expression-result-container.js
+++ src/containers/expression-result-container.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Expression from '../components/expression'
 import Result from '../components/result'
 
-const ExpressionResultContainer = ({ expression, result }) => {
+const ExpressionResultContainer = (props) => {
+  let textStyle = {}
+
+  if (props.input.length > 19) {
+    textStyle = {'fontSize': 22}
+  } else if (props.input.length > 16) {
+    textStyle = {'fontSize': 28}
+  } else if (props.input.length > 12) {
+    textStyle = {'fontSize': 32}
+  }
+
   return (
     <div className='expression-result-container'>
-      <Expression expression={expression} />
-      <Result result={result} />
+      <Expression
+        expression={props.input}
+        style={textStyle} />
+      <Result result={props.result} />
     </div>
   )
 }
 
-export default ExpressionResultContainer
+const mapStateToProps = (state) => ({
+  input: state.input,
+  result: state.result
+})
+
+export default connect(mapStateToProps, null)(ExpressionResultContainer)
