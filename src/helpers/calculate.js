ce6b02931a2f1dd906e7c25e3a3a2fc5f87e0c70
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-03-29 21:03:46 +0100

    Further refactoring

--- src/helpers/calculate.js
+++ src/helpers/calculate.js
@@ -1,53 +1,52 @@
-import operators from '../helpers/operators'
-
-const operatorsArray = ['*', '+', '-', '/']
-
 const badExpression = () => ('Bad Expression')
 
+const add = (a, b) => (a + b)
+const subtract = (a, b) => (a - b)
+const multiply = (a, b) => (a * b)
+const divide = (a, b) => (a / b)
+
+const checkIfNumber = a => isNaN(Number(a))
+
 // Evaluate which mathamtical function should be perfomed based on operator passed
-const callOperator = (operator, a, b) => {
+const sum = (operator, a, b) => {
   switch (operator) {
     case '+':
-      return operators.add(Number(a), Number(b))
+      return add(Number(a), Number(b))
     case '-':
-      return operators.subtract(Number(a), Number(b))
+      return subtract(Number(a), Number(b))
     case '*':
-      return operators.multiply(Number(a), Number(b))
+      return multiply(Number(a), Number(b))
     case '/':
-      return operators.divide(Number(a), Number(b))
+      return divide(Number(a), Number(b))
     default:
       return null
   }
 }
 
-// Loop through array, calling mathmatical operations and passing appropriate values
-const getTotal = (anArray) => {
-  let total = anArray[0] // Initially set total as first element in array
-  for (var i = 0; i < anArray.length; i++) {
-    if (isNaN(Number(anArray[i]))) {
-      if (operatorsArray.includes(anArray[i])) {
-        total = callOperator(anArray[i], total, anArray[i + 1])
-      }
+// Operators must be identified and numbers either side summed
+const getTotal = anArray => {
+  let total = anArray[0]
+  anArray.forEach((item, index) => {
+    if (checkIfNumber(item)) {
+      total = sum(item, total, anArray[index + 1])
     }
-  }
+  })
   return total
 }
 
-// Recreate the array, calculating precedence first
+// Sum precedence operators and recreate the array
 const calculatePrecedence = (anArray) => {
   let arrayCopy = anArray
-  let length = anArray.length
 
-  for (var i = 0; i < length; i++) {
-    let element = anArray[i]
-    if (element === '*' || element === '/') {
-      let total = callOperator(
-        element, anArray[i - 1], anArray[i + 1]
+  arrayCopy.forEach((item, i) => {
+    if (item === '*' || item === '/') {
+      let total = sum(
+        item, anArray[i - 1], anArray[i + 1]
       ).toString()
       arrayCopy.splice(i - 1, 3, total)
       calculatePrecedence(arrayCopy)
     }
-  }
+  })
   return getTotal(arrayCopy)
 }
 
@@ -60,45 +59,38 @@ const checkOperators = (anArray) => {
   }
 }
 
-// Calculate the amount in parens
 const calculateParens = (anArray, op, cp) => {
   let arrayCopy = anArray
-  let end = cp
+  let parenTotal
+  let totalSpliceElements
 
   if (cp === -1) {
-    end = undefined
-  }
-
-  const parenElements = anArray.slice(op + 1, end)
-  const parenTotal = checkOperators(parenElements).toString()
-
-  if (cp === -1) {
-    let arrayStart = anArray.slice(0, op)
-    arrayStart.push(parenTotal)
-    arrayCopy = arrayStart
+    totalSpliceElements = arrayCopy.length - op
+    parenTotal = checkOperators(arrayCopy.slice(op + 1)).toString()
   } else {
-    let totalElements = (cp - op) + 1 // Get # of elements to splice
-    arrayCopy.splice(op, totalElements, parenTotal) // Swap paren elements with their total
+    totalSpliceElements = (cp - op) + 1
+    parenTotal = checkOperators(arrayCopy.slice(op + 1, cp)).toString()
   }
-
+  arrayCopy.splice(op, totalSpliceElements, parenTotal) // Swap paren elements with their total
   return arrayCopy
 }
 
 // If there are parens inside of parens then the innermost parens must be calculated first
 // and then the function should recursively back out, calculating innermost parens as it goes.
 const findParens = (anArray, i) => {
-  const openParen = anArray.indexOf('(', i)
-  const closeParen = anArray.indexOf(')')
+  let openParen = anArray.indexOf('(', i)
+  let closeParen = anArray.indexOf(')', i)
+  let nextParen = anArray.indexOf('(', openParen + 1)
 
-  if (anArray.includes('(') === false) { // Escape and endless loop once all parens calculated
-    return checkOperators(anArray)
-  // If nothing between two parens perform calculation and check for more
-  } else if (anArray.indexOf('(', openParen + 1) > closeParen || anArray.indexOf('(', openParen + 1) === -1) {
-    let newArray = calculateParens(anArray, openParen, closeParen)
-    return findParens(newArray)
-  } else { // Find parens inside parens
-    return findParens(anArray, openParen + 1)
+  if (openParen !== -1) {
+    if (nextParen !== -1) {
+      return findParens(anArray, nextParen)
+    }
+    return findParens(
+      calculateParens(anArray, openParen, closeParen)
+    )
   }
+  return checkOperators(anArray)
 }
 
 // Exported function
@@ -107,12 +99,12 @@ const calculate = (anArray) => {
   let lastElement = arrayCopy[arrayCopy.length - 1]
   let total
 
-  // Trailing operator should be removed
-  if (isNaN(Number(lastElement)) && lastElement !== ')') {
+  // Trailing operator or open paren needs to be removed to allow calculation
+  if (checkIfNumber(lastElement) && lastElement !== ')') {
     arrayCopy.pop()
   }
 
-  // Cannot calculate with more closing paren than opening
+  // Check for more closing paren than opening, less than 3 elements, or parens to be calculated first
   if (arrayCopy.filter(x => x === ')').length > arrayCopy.filter(x => x === '(').length) {
     return badExpression()
   } else if (arrayCopy.length < 3) {
